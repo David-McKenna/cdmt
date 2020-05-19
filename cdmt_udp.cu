f9236de84f036ab2d2366c39a975b4279e8b3751
David McKenna <dmckenna@cp.dias.ie>
2020-05-19 21:59:31 +0100

    Optimise 4-bit unpacking (~3x faster with the define vs previous shift)

--- cdmt_udp.cu
+++ cdmt_udp.cu
@@ -29,6 +29,7 @@ struct header {
 };
 
 
+
 struct header read_sigproc_header(char *fname, char *dataname, int rawudp, int ports);
 void get_channel_chirp(double fcen,double bw,float dm,int nchan,int nbin,int nsub,cufftComplex *c);
 __global__ void transpose_unpadd_and_detect(cufftComplex *cp1,cufftComplex *cp2,int nbin,int nchan,int nfft,int nsub,int noverlap,int nsamp,float *fbuf);
@@ -1203,11 +1204,13 @@ __global__ void decimate(float *z,int ndec,int nchan,int nblock,int nsum,float *
   return;
 }
 
-
 union char_unsigned_int {
     unsigned int ui;
     char c[4];
 };
+
+#define HI_NIBBLE(b) (((b) >> 4) & 0x0F)
+#define LO_NIBBLE(b) ((b) & 0x0F)
 
 int reshapeRawUdp(FILE* rawfile, int packetGulp, int port, int ports, int bitmul, char* udpRawInput, char** udpbuf) {
   int udpPacketLength = UDPPACKETLENGTH * bitmul;
@@ -1222,9 +1225,9 @@ int reshapeRawUdp(FILE* rawfile, int packetGulp, int port, int ports, int bitmul
   
   long currPackNo, lastPackNo;
 
-  unsigned char *bitwork;
+  char *bitwork;
   char *workingInput = udpRawInput;
-  char workingChar;
+  unsigned char workingChar;
 
   union char_unsigned_int ts;
   union char_unsigned_int seq;
@@ -1274,14 +1277,14 @@ int reshapeRawUdp(FILE* rawfile, int packetGulp, int port, int ports, int bitmul
 
 
   if (bitmul == 2) {
-    bitwork = (unsigned char *) malloc(sizeof(unsigned char) * packetGulp * udpPacketLength);
+    bitwork = (char *) malloc(sizeof(char) * packetGulp * udpPacketLength);
     for (i = 0; i < (int) sizeof(char) * packetGulp * UDPPACKETLENGTH; i++) {
         workingChar = udpRawInput[i];
-        bitwork[2 * i]     = (workingChar & 240) >> 4;
-        bitwork[2 * i + 1] = (workingChar & 15);
+        bitwork[2 * i]     = HI_NIBBLE(workingChar);
+        bitwork[2 * i + 1] = LO_NIBBLE(workingChar);
     }
 
-    workingInput = (char *) bitwork;
+    workingInput = bitwork;
   }
 
   
