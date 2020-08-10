9632286c173b5aa3e9f68ff9c443e18f3c751b31
David McKenna <dmckenna@cp.dias.ie>
2020-08-10 13:55:37 +0100

    Split the elapsed time into it's own variable?

--- cdmt_udp.cu
+++ cdmt_udp.cu
@@ -121,6 +121,7 @@ int main(int argc,char *argv[])
   int arg=0;
   FILE **outfile;
   struct timespec tick, tick0, tick1, tock, tock0;
+  double elapsedTime;
 
   lofar_udp_reader *reader;
 
@@ -637,7 +638,8 @@ int main(int argc,char *argv[])
     CLICK(tock);
     printf("Processed %d DMs in %.2f s\n",ndm, TICKTOCK(tick1, tock));
     timeInSeconds += (double) (nread - writeOffset) * timeOffset;
-    printf("Current data processed: %02ld:%02ld:%05.2lf (%1.2lfs) in %1.2lf seconds (%1.2lf/s)\n\n", (long int) (timeInSeconds / 3600.0), (long int) ((fmod(timeInSeconds, 3600.0)) / 60.0), fmod(timeInSeconds, 60.0), timeInSeconds, (double) TICKTOCK(tick, tock), (double) timeInSeconds / (double) TICKTOCK(tick, tock));
+    elapsedTime = (double) TICKTOCK(tick, tock);
+    printf("Current data processed: %02ld:%02ld:%05.2lf (%1.2lfs) in %1.2lf seconds (%1.2lf/s)\n\n", (long int) (timeInSeconds / 3600.0), (long int) ((fmod(timeInSeconds, 3600.0)) / 60.0), fmod(timeInSeconds, 60.0), timeInSeconds, elapsedTime, (double) timeInSeconds / elapsedTime);
     // Exit when we pass the read length limit
     if (total_ts_read > ts_read) {
       break;
