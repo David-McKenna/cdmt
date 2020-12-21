44c7c85b0671e6be3dfb82ca4c9876b5822fc96c
David McKenna <dmckenna@cp.dias.ie>
2020-12-21 16:12:54 +0000

    Remove prints, cuda-memcheck time, I'm out of ideas...

--- cdmt_udp.cu
+++ cdmt_udp.cu
@@ -1177,7 +1177,6 @@ template<typename I> __global__ void unpack_and_padd(I *dbuf0,I *dbuf1,I *dbuf2,
       idx1=ibin+nbin*isub+nsub*nbin*ifft;
       idx2=isub+nsub*(isamp-noverlap);
       cp1[idx1].x=(float) dbuf0[idx2];
-      printf("1: cp1[idx1].x: %f\n", cp1[idx1].x);
       cp1[idx1].y=(float) dbuf1[idx2];
       cp2[idx1].x=(float) dbuf2[idx2];
       cp2[idx1].y=(float) dbuf3[idx2];
@@ -1211,7 +1210,6 @@ template<typename I> __global__ void unpack_and_padd_first_iteration(I *dbuf0,I 
       idx2=isub+nsub*(isamp-noverlap);
 
       cp1[idx1].x=(float) dbuf0[idx2];
-      printf("2: cp1[%ld, %ld].x: %f\n", idx1, idx2, cp1[idx1].x);
       cp1[idx1].y=(float) dbuf1[idx2];
       cp2[idx1].x=(float) dbuf2[idx2];
       cp2[idx1].y=(float) dbuf3[idx2];
@@ -1220,7 +1218,6 @@ template<typename I> __global__ void unpack_and_padd_first_iteration(I *dbuf0,I 
       idx2=isub+nsub*(noverlap-isamp);
 
       cp1[idx1].x=(float) dbuf0[idx2];
-      printf("3: cp1[%ld, %ld].x: %f\n", idx1, idx2, cp1[idx1].x);
       cp1[idx1].y=(float) dbuf1[idx2];
       cp2[idx1].x=(float) dbuf2[idx2];
       cp2[idx1].y=(float) dbuf3[idx2];
@@ -1255,7 +1252,6 @@ template<typename I> __global__ void padd_next_iteration(I *dbuf0,I *dbuf1,I *db
       idx1=ibin+nbin*isub+nsub*nbin*ifft;
       idx2=isub+nsub*(isamp+nsamp-2*noverlap);
       cp1[idx1].x=(float) dbuf0[idx2];
-      printf("4: cp1[idx1].x: %f\n", cp1[idx1].x);
       cp1[idx1].y=(float) dbuf1[idx2];
       cp2[idx1].x=(float) dbuf2[idx2];
       cp2[idx1].y=(float) dbuf3[idx2];
